7aa71b516331fe759290a4f4f53be241d703608c
beta-7 <david.milanovski@students.finki.ukim.mk>
2022-04-20 00:01:41 +0200

    Added button sequence tracking, created queue class. The code has been tested with two microcontrollers talking to each other

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-
+#define debounceRate 50
+#define maxBetweenButtonPresses 5000
 //Stuff needed for executing the LEDs
 //  
 bool ledState=false;
@@ -10,52 +10,69 @@ bool sequenceInProgress=false;
 int currentTiming;
 int targetTiming;
 
+//Stuff needed for capturing the sequence
+//
+bool buttonSequenceStarted = false;
+bool buttonState = false;
+int buttonDelay = 0;
+
+
 
 //Stuff neede for MQTT
-const char* ssid = "xxxxxxxxx";
-const char* password = "x";
-const char* mqtt_server = "18.197.171.34";
+const char* ssid = "DAVID";
+const char* password = "daviddavid2606";
+const char* mqtt_server = "broker.mqttdashboard.com";
 const int mqtt_port = 1883;
-const char* mqtt_sequence_input_topic = "esp198273981273/test/seq/in";
-const char* mqtt_sequence_output_topic = "esp198273981273/test/seq/out";
+const char* mqtt_sequence_output_topic = "esp198273981273/test/seq/in";
+const char* mqtt_sequence_input_topic = "esp198273981273/test/seq/out";
+
 
 //Stuff needed for queueueue
-int timings[100];
-int head=0;
-int tail=0;
-int queueItems=0;
-void queuePush(int num){
-  if(head<100){
+//Isn't fool proof, but the MQTT library will break before this. Good enough for me
+class Queue{
+  private:
+  int timings[100];
+  int head=0;
+  int tail=0;
+  int queueItems=0;
+  public:
+  Queue(){}
+  void queuePush(int num){
+    if(head<100){
     Serial.print("Pushing ");Serial.print(num);Serial.print(" to position ");Serial.println(head);
     timings[head++]=num;
     queueItems++;
   }
-
-}
-bool queueEmpty(){
-  if(queueItems==0){
-    head=0;
-    tail=0;
-    ledState=true;
   }
-  return queueItems==0;
-}
-int queuePop(){
+  bool queueEmpty(){
+    if(queueItems==0){
+      head=0;
+      tail=0;
+      ledState=true;
+    }
+    return queueItems==0;
+  }
+  int queuePop(){
   if(tail<100){
-    Serial.print("Popping");Serial.print(timings[tail]);Serial.print(" from position ");Serial.println(tail);
+    Serial.print("Popping ");Serial.print(timings[tail]);Serial.print(" from position ");Serial.println(tail);
     queueItems--;
     return timings[tail++];
   }
   Serial.println("End of queue!");
   return 0;
 }
+};
+
+
+Queue leds;
+Queue button;
 
 //
 void executeSequence(int pin){
   if(!sequenceInProgress){  
-    digitalWrite(BUILTIN_LED, ledState);
-    if(!queueEmpty()){
-      targetTiming=queuePop();
+    digitalWrite(pin, ledState);
+    if(!leds.queueEmpty()){
+      targetTiming=leds.queuePop();
       sequenceInProgress=true;
       currentTiming=millis();
     }
@@ -111,10 +128,20 @@ void callback(char* topic, byte* payload, unsigned int length) {
   ledState=false;
   while (part!=NULL){
      if(atoi(part)!=0){
-    queuePush(atoi(part));
+    leds.queuePush(atoi(part));
     }
     part = strtok(NULL, ";");
   }
+}
+
+void sendQueueToMQTT(){
+  String outputString="";
+  while(!button.queueEmpty()){
+    outputString += button.queuePop();
+    outputString += ";";
+  }
+  Serial.println(outputString);
+  client.publish(mqtt_sequence_output_topic, outputString.c_str());
 }
 
 void reconnect() {
@@ -142,11 +169,14 @@ void reconnect() {
 }
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(D6,INPUT_PULLUP);
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
+  leds = Queue();
+  button = Queue();
 }
 
 void loop() {
@@ -154,7 +184,35 @@ void loop() {
     reconnect();
   }
   client.loop();
+  executeSequence(LED_BUILTIN);
+  // Serial.println(digitalRead(D6));
 
-  executeSequence(BUILTIN_LED);
-  delay(10);
+  //If the state hasn't been set and the button has been pressed set the state.
+  if(!buttonSequenceStarted && !digitalRead(D6)){
+    buttonDelay=millis();
+    buttonSequenceStarted = true;
+    buttonState = digitalRead(D6);
+  }
+  //If the time between button presses is less than 5 seconds debounce it and send it to the queue,
+  //else send the queue contents through MQTT and reset the state.
+  if(buttonSequenceStarted && digitalRead(D6)!= buttonState){
+    buttonState = digitalRead(D6);
+    if(millis()-buttonDelay < maxBetweenButtonPresses){
+      if(millis() - buttonDelay > debounceRate){
+          button.queuePush(millis()-buttonDelay);
+      }
+          buttonDelay = millis();
+    }
+    else{
+    buttonSequenceStarted = false;
+    sendQueueToMQTT();
+    }
+  }
+  //If the sequence was started, the button isn't held down and the time between presses is more than the set
+  //stop the sequence and send the queue contents through MQTT.
+  if(buttonSequenceStarted && digitalRead(D6) && millis()-buttonDelay > maxBetweenButtonPresses){
+      buttonSequenceStarted = false;
+      sendQueueToMQTT();
+  }
+
 }
